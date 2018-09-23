a3ceb673670e51dfe6ad9963a9dbfa3da65e4e5a
Viktor Karpilov <wktorion@gmail.com>
2018-09-23 19:39:06 +0300

    Recursion task (11)

--- 11/first.py
+++ 11/first.py
@@ -1,2 +1,31 @@
 import validation as val
-prit(val.int_valid(3))
+def entering():
+    number = input("Enter number:")
+    answer = input("Want to exit ?('yes',something else)")
+    if(not val.int_valid(number)):
+        print("Enter sommething that more 'intrative' =)")
+        return entering()
+    else:
+        if(answer == "yes" or answer == "Yes"):
+            return number
+        return number+"/"+entering()
+    return "0"
+def bigger(numbers_set):
+    number = int(numbers_set.pop())
+    second_number = False
+    if(len(numbers_set)!=0):
+        second_number = bigger(numbers_set)
+    if(second_number and number<second_number):
+        print(number<second_number)
+        print(str(second_number)+"B")
+        print(str(number)+"A")
+        return second_number
+    else:
+        print(number)
+        return number
+
+numbers_string = entering()
+numbers_set = set(numbers_string.split("/"))
+print(bigger(numbers_set))
+
+
